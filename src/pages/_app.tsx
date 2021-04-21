ea9d69fb9b7007ccb40ae3407ed2ae2afe67dd72
frederico.mngs <frederico@mngs.com.br>
2021-04-21 20:30:13 -0300

    Aula 3

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,7 @@
 import "../styles/global.scss";
-import React from "react";
-import styles from "../styles/app.module.scss";
 import { Header } from "../components/Header";
 import { Player } from "../components/Player";
+import styles from "../styles/app.module.scss";
 
 function MyApp({ Component, pageProps }) {
   return (
